e76f3276e872ebfa14b9b0c2ed3ae24a863111ec
Refusion <refusion99@gmail.com>
2024-03-15 10:14:11 +0200

    education site

---
+++ src/education/settings/prod.py
@@ -0,0 +1,29 @@
+from .base import *
+import os
+
+DEBUG = False
+
+ADMINS = [
+    ('Roman O', 'refusion99@gmail.com'),
+]
+
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
